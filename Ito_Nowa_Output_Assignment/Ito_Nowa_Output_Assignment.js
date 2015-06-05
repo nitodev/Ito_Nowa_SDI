e226e8e40ed0ee1229566d9fa5f211698bd4fb72
nitodev <Naito@Nowas-MacBook-Pro.local>
2015-06-04 19:56:56 -0500

    Final Update 5

--- Ito_Nowa_Output_Assignment/Ito_Nowa_Output_Assignment.js
+++ Ito_Nowa_Output_Assignment/Ito_Nowa_Output_Assignment.js
@@ -2,6 +2,9 @@
 // SDI Section 1, June
 // Output Assignment
 
-var married = false;
-var numberOfSiblings = 3;
-var countryOfBirth = "Japan";
+var married = false;  //am I married?
+var numberOfSiblings = 3;  //3 sisters
+var countryOfBirth = "Japan";  //Born in Japan
+
+
+console.log("I am not married, has three sisters, and born in Japan.");
