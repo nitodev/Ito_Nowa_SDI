b574e5039ae9e72e6ec5bf1445d4d9036b035481
nitodev <Naito@Nowas-MacBook-Pro.local>
2015-06-11 00:01:38 -0500

    expression worksheet v2

--- Expressions_worksheet/worksheet.js
+++ Expressions_worksheet/worksheet.js
@@ -3,7 +3,20 @@
 	Expressions Worksheet
 */
 
+/*
+// Dog Years
 var sparkyAge = prompt("How old is Sparky?"); // input age in human years
 var age = Number(sparkyAge) * 7; // age input * 7
 alert("Sparky is " + age + " in dog years."); // output calculation
 console.log("Sparky is " + sparkyAge + " human years old, which is " + age + " in dog years.");
+*/
+
+
+// Slice of Pie part 1
+var numberOfPizza = prompt("How many pizza was ordred?"); // input for # of pizza ordered
+var numberOfSlices = prompt("How many slices per pizza?"); // input for # of slices per pizza
+var people = prompt("How many people are at the party?"); // input for # of people present
+var result = Math.round(Number(numberOfPizza * numberOfSlices) / Number(people)); // rounded value of calculation
+alert("Each person can have " + result + " slices."); // output/alert for the result
+console.log("Each person ate " + result + " slices of pizza at the party."); 
+
