42e13dd2c2815ad3fc8beca5c3598fecd14d5975
nitodev <Naito@Nowas-MacBook-Pro.local>
2015-06-11 04:32:13 -0500

    expressions worksheet v5

--- Expressions_worksheet/worksheet.js
+++ Expressions_worksheet/worksheet.js
@@ -11,6 +11,7 @@ alert("Sparky is " + age + " in dog years."); // output calculation
 console.log("Sparky is " + sparkyAge + " human years old, which is " + age + " in dog years.");
 
 
+//------------------------------help--------------------------------------
 
 // Slice of Pie part 1
 
@@ -30,29 +31,29 @@ var remainder = result % 1;
 var leftOver = Number(remainder) * Number(numberOfPizza * numberOfSlices);
 alert(leftOver);
 
-
+//-----------------------------------------------------------------------
 
 
 // Average shopping bill
-var setArray = [prompt("week 1 spending"), prompt("week 2 spending"), prompt("week 3 spending"), prompt("week 4 spending"), prompt("week 5 spending")];
-var total = (Number(setArray[0]) + Number(setArray[1]) + Number(setArray[2]) + Number(setArray[3]) + Number(setArray[4]))
-var average = total / setArray.length;
-alert("Total amount spend on groceries is $" + total); 
-alert("The average weekly spending on grocery is $" + average);
+var setArray = [prompt("week 1 spending"), prompt("week 2 spending"), prompt("week 3 spending"), prompt("week 4 spending"), prompt("week 5 spending")]; // arrays with prompts for input of $$
+var total = (Number(setArray[0]) + Number(setArray[1]) + Number(setArray[2]) + Number(setArray[3]) + Number(setArray[4])); // total $ for 5 weeks
+var average = total / setArray.length; // total divided by the length of array (5)
+alert("Total amount spend on groceries is $" + total); // result of total
+alert("The average weekly spending on grocery is $" + average); // result of average
 console.log("You have spent a total of $" + total + " on groceries over 5 weeks. That is an average of $" + average + " per week.");
 
 
 
 // Discounts
-var item = prompt("What is your item?");
-var tag = prompt("What is the price on tag?");
-var discount = prompt("What is the discount percentage? Excluding '%'");
-var tax = prompt("What is the sales tax percentage? Excluding '%'");
+var item = prompt("What is your item?"); // input for item name
+var tag = prompt("What is the price on tag?"); // input for original $
+var discount = prompt("What is the discount percentage? Excluding '%'"); // input for discount %
+var tax = prompt("What is the sales tax percentage? Excluding '%'"); // input for tax %
 
-var discountPerc = Number(discount * .01);
-var taxPerc = Number(tax * .01);
+var discountPerc = Number(discount * .01); // converting % to decimal
+var taxPerc = Number(tax * .01); // converting % to decimal
 
-var priceWithDisc = Number(tag) - Number(tag * discountPerc);
-var priceWithTax = Number(priceWithDisc * taxPerc) + priceWithDisc;
+var priceWithDisc = Number(tag) - Number(tag * discountPerc); // price with discount calculated
+var priceWithTax = Number(priceWithDisc * taxPerc) + priceWithDisc; // price with discount + tax calculated
 alert("Your " + item + " was originally $" + tag + ", but after a " + discount + "% discount, it is now $" + priceWithDisc + " without tax, and $" + priceWithTax + " with tax.");
 console.log("Your " + item + " was originally $" + tag + ", but after a " + discount + "% discount, it is now $" + priceWithDisc + " without tax, and $" + priceWithTax + " with tax.")
