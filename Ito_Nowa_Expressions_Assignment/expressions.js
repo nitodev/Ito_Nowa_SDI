7f7c79bea54bf072c311bf2436547356b77fd1d3
nitodev <Naito@Nowas-MacBook-Pro.local>
2015-06-11 22:03:45 -0500

    expression assignment v2

--- Ito_Nowa_Expressions_Assignment/expressions.js
+++ Ito_Nowa_Expressions_Assignment/expressions.js
@@ -4,7 +4,11 @@
 	Expressions Assignment
 */
 
-alert("test");
+var array1 = [prompt("Type your first value for multiplication:"), prompt("Type your second value for multiplication:")];
+var math = Number(array1[0]) * Number(array1[1]);
+alert("The answer is " + math);
+var step2 = prompt("Type your third value for multiplication if applicable. If not, disregard.");
+math *= Number(step2);
+alert("The answer is " + math);
+console.log("The total value by multiplication is " + math);
 
-var array1 = [prompt("Type your first value:"), prompt("Type your second value:")]
-var decision1 = prompt("Input the symbol you wish to operate (i.e. +, -, *, /)")
