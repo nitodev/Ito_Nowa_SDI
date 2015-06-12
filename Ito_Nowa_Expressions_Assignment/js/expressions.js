98e0110a68ca91dc04083c62b58e99703a228b87
nitodev <Naito@Nowas-MacBook-Pro.local>
2015-06-11 22:31:02 -0500

    expressions assignment v6

--- Ito_Nowa_Expressions_Assignment/js/expressions.js
+++ Ito_Nowa_Expressions_Assignment/js/expressions.js
@@ -4,15 +4,15 @@
 	Expressions Assignment
 */
 //multiplication 
-var array1 = [prompt("Input the speed in MPH:"), prompt("Input your time of travel in hours:")];
-var math = Number(array1[0]);
-math *= Number(array1[1]);
-alert("You will travel " + math + " Miles.");
-console.log("The total distance traveled is " + math + " Miles!");
+var array1 = [prompt("Input the speed in MPH:"), prompt("Input your time of travel in hours:")]; // input for mph and hours of travel time
+var math = Number(array1[0]); // assigning array1[0] to a variable
+math *= Number(array1[1]); // using multiplication operator
+alert("You will travel " + math + " Miles."); // alerting the result
+console.log("The total distance traveled is " + math + " Miles!"); // logging the result
 
 
 //addition
-var array2 = [prompt("Type your first value for the side of triangle in 'cm':"), prompt("Type your second value for the side of triangle in 'cm':"), prompt("Type your third value for the side of triangle in 'cm':")];
-var math2 = Number(array2[0]) + Number(array2[1]) + Number(array2[2]);
-alert("The perimeter of the triangle is " + math2 + "cm.");
-console.log("The perimeter of the triangle is " + math2 + "cm.");
+var array2 = [prompt("Type your first value for the side of triangle in 'cm':"), prompt("Type your second value for the side of triangle in 'cm':"), prompt("Type your third value for the side of triangle in 'cm':")]; // input for the data of triangle
+var math2 = Number(array2[0]) + Number(array2[1]) + Number(array2[2]); // adding the numbers for perimeter
+alert("The perimeter of the triangle is " + math2 + "cm."); // alert for the result
+console.log("The perimeter of the triangle is " + math2 + "cm."); // logging the result
