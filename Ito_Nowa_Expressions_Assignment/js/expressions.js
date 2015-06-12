a83026378c2dfe05c820d22fce3e8fd4309b4efa
nitodev <Naito@Nowas-MacBook-Pro.local>
2015-06-11 22:55:01 -0500

    expressions assignment final v7

--- Ito_Nowa_Expressions_Assignment/js/expressions.js
+++ Ito_Nowa_Expressions_Assignment/js/expressions.js
@@ -13,6 +13,7 @@ console.log("The total distance traveled is " + math + " Miles!"); // logging th
 
 //addition
 var array2 = [prompt("Type your first value for the side of triangle in 'cm':"), prompt("Type your second value for the side of triangle in 'cm':"), prompt("Type your third value for the side of triangle in 'cm':")]; // input for the data of triangle
-var math2 = Number(array2[0]) + Number(array2[1]) + Number(array2[2]); // adding the numbers for perimeter
+var math2 = Number(array2[0]) + Number(array2[1]); // adding the numbers for perimeter
+math2 += Number(array2[2]);
 alert("The perimeter of the triangle is " + math2 + "cm."); // alert for the result
 console.log("The perimeter of the triangle is " + math2 + "cm."); // logging the result
