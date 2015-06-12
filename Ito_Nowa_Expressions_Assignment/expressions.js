439caeced662757aa6f2a0cb6d9ad8d5933f4671
nitodev <Naito@Nowas-MacBook-Pro.local>
2015-06-11 20:52:33 -0500

    expressions assignment

---
+++ Ito_Nowa_Expressions_Assignment/expressions.js
@@ -0,0 +1,10 @@
+/* Nowa Ito
+	6/11/2015
+	SDI Section 1
+	Expressions Assignment
+*/
+
+alert("test");
+
+var array1 = [prompt("Type your first value:"), prompt("Type your second value:")]
+var decision1 = prompt("Input the symbol you wish to operate (i.e. +, -, *, /)")
