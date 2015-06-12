0c8049128c4694c7620603b2a021873bed9ee903
nitodev <Naito@Nowas-MacBook-Pro.local>
2015-06-11 22:25:41 -0500

    expression assignment v5

--- Ito_Nowa_Expressions_Assignment/js/expressions.js
+++ Ito_Nowa_Expressions_Assignment/js/expressions.js
@@ -3,12 +3,16 @@
 	SDI Section 1
 	Expressions Assignment
 */
+//multiplication 
+var array1 = [prompt("Input the speed in MPH:"), prompt("Input your time of travel in hours:")];
+var math = Number(array1[0]);
+math *= Number(array1[1]);
+alert("You will travel " + math + " Miles.");
+console.log("The total distance traveled is " + math + " Miles!");
 
-var array1 = [prompt("Type your first value for multiplication:"), prompt("Type your second value for multiplication:")];
-var math = Number(array1[0]) * Number(array1[1]);
-alert("The answer is " + math);
-var step2 = prompt("Type your third value for multiplication if applicable. If not, disregard.");
-math *= Number(step2);
-alert("The answer is " + math);
-console.log("The total value by multiplication is " + math);
 
+//addition
+var array2 = [prompt("Type your first value for the side of triangle in 'cm':"), prompt("Type your second value for the side of triangle in 'cm':"), prompt("Type your third value for the side of triangle in 'cm':")];
+var math2 = Number(array2[0]) + Number(array2[1]) + Number(array2[2]);
+alert("The perimeter of the triangle is " + math2 + "cm.");
+console.log("The perimeter of the triangle is " + math2 + "cm.");
