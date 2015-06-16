31ac583f9e3fafba5986d2906f75078361f5d2a0
nitodev <Naito@Nowas-MacBook-Pro.local>
2015-06-16 18:54:17 -0500

    conditionals v2

--- Conditionals_worksheet/conditionals.js
+++ Conditionals_worksheet/conditionals.js
@@ -6,10 +6,17 @@ var temperature = Number(prompt("What is the temperature without the unit?"));
 var unit = prompt("What unit is the temperature in? Celsius (C) or Fahrenheit (F)");
 /*
 if(unit == "C" || unit == "c"){
-	console.log("The temperature is " + temperature + " degrees in Celsius.");
+	temperature = temperature * 9 / 5 + 32
+	console.log("The converted temperature from Celsius is " + temperature + " degrees in Fahrenheit.");
 }else if(unit == "F" || unit == "f"){
-	console.log("The temperature is " + temperature + " degrees in Fahrenheit.");
+	temperature = (temperature - 32) * 5 / 9
+	console.log("The converted temperature from Fahrenheit is " + temperature + " degrees in Celsius.");
 }
 */
+
+
 unit = ("C" || "c") ? "The temperature is " + temperature + " degrees in Celsius." : "The temperature is " + temperature + " degrees in Fahrenheit.";
 console.log(unit);
+
+/////
+
